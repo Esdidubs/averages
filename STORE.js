f16e56704eebfaf65bc198fd4aef2fd968d101cd
Esdidubs <sward3644@gmail.com>
2020-11-25 21:49:12 -0700

    added more questions

--- STORE.js
+++ STORE.js
@@ -23,5 +23,101 @@ const STORE = [
         question: 'Longest field goal in yards for the NFL',
         answer: '64'
     },
+    {
+        question:"Year that Nintendo became a company",
+        answer:"1889"
+    },
+    {
+        question:"The number of years that the Hundred Years War lasted",
+        answer:"116"
+    },
+    {
+        question:"The amount of sugar in grams in a 2L bottle of Coca Cola",
+        answer:"234"
+    },
+    {
+        question:"Height in inches of Danny Devito",
+        answer:"58"
+    },
+    {
+        question:"Height in feet of the Eiffel Tower",
+        answer:"1063"
+    },
+    {
+        question:"Height in feet of the Statue of Liberty",
+        answer:"305"
+    },
+    {
+        question:"Weight in pounds of the heaviest man ever recorded",
+        answer:"1400"
+    },
+    {
+        question:"Number of chapters in the Bible",
+        answer:"1189"
+    },
+    {
+        question:"Number of unique words in Green Eggs and Ham",
+        answer:"50"
+    },
+    {
+        question:"Distance in air miles between San Francisco and New York City",
+        answer:"2572"
+    },
+    {
+        question:"Runtime in minutes of Titanic",
+        answer:"194"
+    },
+    {
+        question:"Number of films in The Land Before Time series",
+        answer:"14"
+    },
+    {
+        question:"Number of letters in the Khmer (Cambodian) alphabet",
+        answer:"74"
+    },
+    {
+        question:"Number of MASH episodes",
+        answer:"251"
+    },
+    {
+        question:"Length of world's longest cake, in feet",
+        answer:"17388"
+    },
+    {
+        question:"Longest cucumber, in inches",
+        answer:"42.1"
+    },
+    {
+        question:"Most total children from one mother",
+        answer:"69"
+    },
+    {
+        question:"Most toothpicks stuck into a single beard",
+        answer:"3500"
+    },
+    {
+        question:"Height in feet of the Washington Monument",
+        answer:"555"
+    },
+    {
+        question:"Oldest codified (contained in a single document) constitution, in years",
+        answer:"233"
+    },
+    {
+        question:"Number of elements on periodic table",
+        answer:"118"
+    },
+    {
+        question:"In 79 years, how many times does Mars orbit the sun?",
+        answer:"42"
+    },
+    {
+        question:"Number of books in the original Goosebumps series",
+        answer:"62"
+    },
+    {
+        question:"Population of Vatican City as of 2019",
+        answer:"825"
+    }
     
 ];
